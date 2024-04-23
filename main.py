5a5ad83d3d2ed1633080702d0a578de4ebcf2449
SavionW-Works <72318904+SavionW-Works@users.noreply.github.com>
2024-04-23 13:06:06 -0400

    Fixed CORS Exceptions

--- main.py
+++ main.py
@@ -3,17 +3,26 @@
 """
 
 from typing import List
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
+
+
+from lib import department_crud 
+from lib import employee_crud 
+from lib import hospital_crud  
+from lib import insurance_crud 
+from lib import medication_crud
 from lib import patient_crud 
-from lib import physician_crud
+from lib import physician_crud 
+from lib import prescription_crud  
+
 from lib import response_models
 from lib.database_connection import SessionLocal
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI() 
 
@@ -21,21 +30,22 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "https://localhost:8000/patients", 
+    "http://localhost:3000/patients",  
     "https://localhost:3000/patients", 
-    "https://localhost:5432/patients",
+    "localhost:3000"
 ] 
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 ) 
 # Dependency
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal() 
+    
     try:
         yield db
     finally:
@@ -54,8 +64,16 @@ def get_patients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 @app.get("/physician/", response_model=List[response_models.Physician])
 def get_patients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     physicians = physician_crud.get_physicians(db, skip=skip, limit=limit)
+    return physicians  
+
+@app.get("/prescription/", response_model=List[response_models.Physician])
+def get_patients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    physicians = physician_crud.get_physicians(db, skip=skip, limit=limit)
     return physicians 
+
+
+
 
 #CORS allowed origins
 
-
+# python -m uvicorn main:app --reload
